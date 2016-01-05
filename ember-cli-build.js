d5c8b6663d84e7baa940df914c318ae7d93c3ad4
Chris Manson <chris@manson.ie>
2016-01-05 17:34:37 +0000

    moving Brocfile to ember-cli-build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,14 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  app.import('app/styles/reset.css');
+  app.import('app/styles/alert.css');
+  app.import('bower_components/perfect-scrollbar/css/perfect-scrollbar.css');
+  app.import('bower_components/jquery-jsonview/dist/jquery.jsonview.css');
+  app.import('bower_components/lodash/dist/lodash.js');
+  app.import('bower_components/jquery-jsonview/dist/jquery.jsonview.js');
+  app.import('bower_components/perfect-scrollbar/js/perfect-scrollbar.jquery.js');
+
   // Use `app.import` to add additional libraries to the generated
   // output files.
   //
