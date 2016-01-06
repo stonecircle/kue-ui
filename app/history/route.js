954484f270037beca03acb94130b17f81d75d1c9
Chris Manson <chris@manson.ie>
2016-01-06 21:13:14 +0000

    re-arranging the api and moving JSON:API endpoints to a v2 api

--- app/history/route.js
+++ app/history/route.js
@@ -3,6 +3,6 @@ import moment from 'moment';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.query('stat', {since: moment().subtract(1, 'days').toDate().getTime()});
+    return this.store.query('state-timebucket', {since: moment().subtract(1, 'days').toDate().getTime()});
   }
 });
