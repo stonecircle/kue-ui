8be372a6d1c1d6a3a72f9fcd2bbcb5bf43603e4b
Chris Manson <chris@manson.ie>
2016-01-06 12:46:48 +0000

    using ember-data for stats

--- app/history/controller.js
+++ app/history/controller.js
@@ -4,7 +4,6 @@ import moment from 'moment';
 export default Ember.Controller.extend({
   chartOptions: Ember.computed(function() {
     return {
-
       chart: {
         type: 'column'
       },
@@ -12,9 +11,8 @@ export default Ember.Controller.extend({
         text: 'Jobs by hour'
       },
       xAxis: {
-        categories: Object.keys(this.get('model.complete')).map(function(item){
-          console.log(item);
-          return moment(parseInt(item)).format('ddd HH');
+        categories: this.get('model').map(function(stat){
+          return moment(parseInt(stat.id)).format('ddd HH');
         })
       },
       yAxis: {
@@ -22,18 +20,19 @@ export default Ember.Controller.extend({
           text: 'Job count'
         }
       }
-    }
+    };
   }),
-  chartData: Ember.computed('model.complete.@each.length', 'model.failed.@each.length', function() {
+
+  chartData: Ember.computed('model.@each.complete.[]', 'model.@each.failed.[]', function() {
     return [{
       name: 'Complete',
-      data: Object.keys(this.get('model.complete')).map((item) => {
-        return this.get(`model.complete.${item}.length`);
+      data: this.get('model').map((stat) => {
+        return stat.get('complete');
       })
     }, {
       name: 'Failed',
-      data: Object.keys(this.get('model.failed')).map((item) => {
-        return this.get(`model.failed.${item}.length`);
+      data: this.get('model').map((stat) => {
+        return stat.get('failed');
       })
     }];
   }),
