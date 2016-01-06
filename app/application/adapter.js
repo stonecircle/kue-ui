8e0d187fb06f0f38952d4134d4b777036aae94d9
Chris Manson <chris@manson.ie>
2016-01-06 21:13:14 +0000

    re-arranging the api and moving JSON:API endpoints to a v2 api

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 import config from 'client/config/environment';
 
 export default DS.JSONAPIAdapter.extend({
-  host: config.apiURL,
+  host: `${config.apiURL}/v2`,
 });
