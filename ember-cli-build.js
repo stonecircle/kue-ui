b346dbba439881feb505072a999d9b5152115735
Chris Manson <chris@stonecircle.io>
2018-07-16 10:44:23 +0100

    fixing update

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    // Add options here
+    'ember-cli-babel': {
+      includePolyfill: true,
+    },
   });
 
   app.import('node_modules/perfect-scrollbar/css/perfect-scrollbar.css');
